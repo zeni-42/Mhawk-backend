1ec96926a999f73b5dedafb61c7933764b2ac83d
zeni-42 <mailzeni42@gmail.com>
2025-07-29 11:46:01 +0530

    Repository fixed

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"log"
-	"os"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,32 +10,45 @@ import (
 
 func InitTables(client *pgxpool.Pool) {
 	if client == nil {
-		log.Println("DB client is nil can't proceed")
-		os.Exit(1)
+		log.Fatalf("DB client is nil can't proceed")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	query := 
-	`CREATE TABLE users (
-		id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-		fullname TEXT NOT NULL,
-		email TEXT UNIQUE NOT NULL,
-		password TEXT NOT NULL,
-		refresh_token TEXT,
-		free_token INTEGER DEFAULT 50,
-		is_pro BOOLEAN DEFAULT FALSE,
-		is_organization BOOLEAN DEFAULT FALSE,
-		api_id UUID,
-		organization_id UUID,
-		created_at TIMESTAMPTZ DEFAULT NOW(),
-		updated_at TIMESTAMPTZ DEFAULT NOW()
-	);`
+	var exist bool
+	err := client.QueryRow(ctx, `
+		SELECT EXISTS (
+			SELECT FROM information_schema.tables 
+			WHERE table_schema = 'public' 
+			AND table_name = 'users'
+		);
+	`).Scan(&exist)
 
-
-	_ , err := client.Exec(ctx, query)
 	if err != nil {
-		log.Fatalf("Failed to create table: %v", err)
+		log.Fatalf("Failed to check if 'users' table exists: %v", err)
+	}
+
+	if !exist {
+		query := 
+		`CREATE TABLE users (
+			id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+			fullname TEXT NOT NULL,
+			email TEXT UNIQUE NOT NULL,
+			password TEXT NOT NULL,
+			refresh_token TEXT,
+			free_token INTEGER DEFAULT 50,
+			is_pro BOOLEAN DEFAULT FALSE,
+			is_organization BOOLEAN DEFAULT FALSE,
+			api_id UUID,
+			organization_id UUID,
+			created_at TIMESTAMPTZ DEFAULT NOW(),
+			updated_at TIMESTAMPTZ DEFAULT NOW()
+		);`
+		
+		_ , err := client.Exec(ctx, query)
+		if err != nil {
+			log.Fatalf("Failed to create table: %v", err)
+		}
 	}
 }
